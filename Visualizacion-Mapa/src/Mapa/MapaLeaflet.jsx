2b6ff62ffec22a2fe6c9de62f0d0618734d509b0
Claudio Huerta <claudio.huertat@sansano.usm.cl>
2025-03-08 23:00:30 -0300

    agregamos un tooltip a cada marcador del mapa

--- Visualizacion-Mapa/src/Mapa/MapaLeaflet.jsx
+++ Visualizacion-Mapa/src/Mapa/MapaLeaflet.jsx
@@ -3,15 +3,13 @@ import { InstitucionesContext } from '../context/InstitucionesContext';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-
-
 const MapaLeaflet = () => {
   const { instituciones, setInstitucionSeleccionada } = useContext(InstitucionesContext);
-  const {mapCenter,setMapCenter} = useContext(InstitucionesContext);
+  const { mapCenter, setMapCenter } = useContext(InstitucionesContext);
 
   useEffect(() => {
     if (instituciones && Object.keys(instituciones).length > 0) {
-      const map = L.map('map').setView(mapCenter, 4); // Centrado en Santiago, Chile
+      const map = L.map('map').setView(mapCenter, 4); // Centrado en el mapa con el nivel de zoom 4
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap contributors',
@@ -23,7 +21,19 @@ const MapaLeaflet = () => {
         if (geolocalizacion && geolocalizacion.latitude && geolocalizacion.longitude) {
           const { latitude: lat, longitude: lng } = geolocalizacion;
 
+          // Crear el contenido del tooltip
+          const tooltipContent = `
+            <strong>${nombre}</strong><br>
+            ${geolocalizacion.city ? `Ciudad: ${geolocalizacion.city}<br>` : ''}
+            ${geolocalizacion.region ? `Región: ${geolocalizacion.region}<br>` : ''}
+            País: ${geolocalizacion.country}
+          `;
+
+          // Crear el marcador y agregar el tooltip
           const marker = L.marker([lat, lng]).addTo(map);
+          marker.bindTooltip(tooltipContent, { permanent: false, direction: 'top' });
+
+          // Evento al hacer clic en el marcador
           marker.on('click', () => {
             setInstitucionSeleccionada({ nombre, trabajos });
           });
@@ -34,7 +44,7 @@ const MapaLeaflet = () => {
 
       return () => map.remove();
     }
-  }, [instituciones, setInstitucionSeleccionada]);
+  }, [instituciones, setInstitucionSeleccionada, mapCenter]);
 
   return <div id="map" style={{ height: '700px', width: '40%' }} />;
 };
